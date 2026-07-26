b87395b01e9c0e7a3dcfe86890658649d39e06f0
Asaf Arusi <asafarusi2@gmail.com>
2026-07-26 19:46:13 +0300

    Test pipeline trigger

--- GITrepo-ex3-AsafArusi/MyName.java
+++ GITrepo-ex3-AsafArusi/MyName.java
@@ -1,5 +1,5 @@
 public class MyName {
     public static void main(String[] args) {
-        System.out.println("I'm Asaf Arusi and I'm learning jenkins :)");
+        System.out.println("I'm Asaf Arusi and I'm learning jenkins:)");
     }
 }
