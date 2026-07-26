d9d8dc08273ee6dc64dd1aac4e710afac2f2b95e
Asaf Arusi <asafarusi2@gmail.com>
2026-07-26 18:19:36 +0300

    modified hello.java to follow the ci/cd on jenkins

--- FourthLecture/Hello.java
+++ FourthLecture/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!\n");
+        System.out.println("I just made a little change...")
     }
 }
